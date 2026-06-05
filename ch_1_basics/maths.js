961504aaa8c06336806cbf07081869343ae72db0
Kaushal-tech <kaushalvivek2005@gmail.com>
2026-06-05 18:37:52 +0000

    number and maths are practiced

---
+++ ch_1_basics/maths.js
@@ -0,0 +1,15 @@
+// console.log(Math)
+// console.log(Math.abs(+10))  //provides absolute value by removing its sign
+
+// console.log(Math.round(3.85))
+// console.log(Math.ceil(3.85))    //provides upper value even if slightely rise in decimal part 
+// console.log(Math.floor(3.85))   //takes lower value ignoring decimal part
+
+// console.log(Math.min(3, 7, 1, 20))
+// console.log(Math.max(3, 7, 1, 20))
+
+console.log(Math.floor(Math.random()*10)+1) // provide value between 1 & 10
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min+1))+min)  // value between 10 & 20
---
+++ ch_1_basics/number.js
@@ -0,0 +1,2 @@
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'))
