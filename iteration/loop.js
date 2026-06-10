175bc8ba0a2f5c2b9c4e3773dfea04864fb400f6
Kaushal-tech <kaushalvivek2005@gmail.com>
2026-06-10 12:32:14 +0000

    for loop is studied

---
+++ iteration/loop.js
@@ -0,0 +1,28 @@
+//for
+// for (let i = 1; i <=10; i++) {
+//     console.log(`Outer loop value: ${i}`)
+//     for (let j = 1; j <=10; j++) {
+//         //console.log(`inner loop value ${j} and inner loop ${i}`);
+//         console.log(i + '*' + j + '=' + i*j)
+//     }
+    
+// }
+
+let myArray = ["flash","ironman", "spiderman"]
+// console.log(myArray.length)
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+}
+
+
+//break(skip the whole loop) and continue(skip, current iteration only)
+for (let index = 1; index <= 20; index++) {
+    if (index == 5){
+        console.log("5 is best no")
+        //break
+        continue
+    }
+    console.log(`Value of i is ${index}`)
+    
+}
