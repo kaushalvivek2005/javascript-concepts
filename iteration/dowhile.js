dbbb7973455092d6135048aaea2fcd0e523e9a2d
Kaushal-tech <kaushalvivek2005@gmail.com>
2026-06-10 13:41:50 +0000

    loops completed

---
+++ iteration/dowhile.js
@@ -0,0 +1,9 @@
+//do while loop
+
+//let score = 11
+let score = 1
+
+do{
+    console.log(`score is ${score}`)
+    score++
+} while(score <= 10)
---
+++ iteration/while.js
@@ -0,0 +1,12 @@
+let index =0
+while (index <=10) {
+    // console.log(`Value of index is ${index}`)
+    index = index + 2
+}
+
+let myArray  = ['flash','batman','ironman']
+let i = 0
+while (i < myArray.length){
+    console.log(`Value is ${myArray[i]}`)
+    i = i +1
+}
