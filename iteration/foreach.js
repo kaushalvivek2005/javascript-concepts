fbfb9ad21b625052fb649875720271d509e87888
Kaushal-tech <kaushalvivek2005@gmail.com>
2026-06-10 16:04:38 +0000

    foreach loop is studied

--- iteration/foreach.js
+++ iteration/foreach.js
@@ -1,3 +1,37 @@
 const coding = ["js", "ruby", "c", "cpp", "python"]
 
-coding.for
+coding.forEach(function (val){
+    // console.log(val)
+})
+
+coding.forEach((val)=>{
+    //console.log(val)
+})
+
+// function printMe(item){
+//     console.log(item)
+// }
+// coding.forEach(printMe)
+
+coding.forEach((item, index, array)=>{
+    // console.log(item, index, array)
+})
+
+const myCoding = [{
+    languageName: "javascript",
+    languageFileName: "js"
+},
+{
+    languageName: "java",
+    languageFileName: "java"
+},
+{
+    languageName: "python",
+    languageFileName: "py"
+}
+]
+
+myCoding.forEach((item)=>{
+    console.log(item.languageFileName)
+    console.log(item.languageName)
+})
